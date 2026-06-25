6bbbe848396bfcdf9571ad850fd7a7f9eb023966
Sanskar Sovitkar <sanskarsovitkar05@gmail.com>
2026-06-25 13:59:54 +0530

    Fix loop condition in isSolved method
    Updated the loop condition in isSolved method to check 3 layers instead of 4.

--- Model/Cube_3D_Representation.cpp
+++ Model/Cube_3D_Representation.cpp
@@ -57,7 +57,7 @@ public:
         // basic condition
         for(int i=0;i<6;i++){
             for(int j=0;j<3;j++){
-                for(int k=0;k<4;k++){
+                for(int k=0;k<3;k++){
                     if(this->cube[i][j][k] != getColorChar(Color(i))) return false;     // not matching
                 }   
             }
